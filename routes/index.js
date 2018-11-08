85922923c35a6598e5b03251ed1be3a53e4d48a8
Daniel Lopez <dlopez@info-arch.com>
2018-11-08 11:16:44 -0400

    Added random datasets from server Fixed responsive layout Code refactored

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
+var timeFormat = 'MM/DD/YYYY';
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' });
+	res.render('index',
+		{
+			title: 'Agile Automation Monitor',
+			dataSets: {
+				automator1: getRandomDataSet(10),
+				automator2: getRandomDataSet(10)
+			}
+		});
 });
+
+function newDateString(days) {
+	return moment().add(days, 'd').format(timeFormat);
+}
+
+function getRandomNumber(){
+	return Math.floor(Math.random() * -10) - 10;
+}
+
+function getRandomDataSet(dataNumbers){
+	var dataset=[];
+	for (var i = 0; i < dataNumbers; i++){
+		dataset.push({
+			x: newDateString(i),
+			y: getRandomNumber()
+		})
+	}
+	return dataset
+}
 
 module.exports = router;
